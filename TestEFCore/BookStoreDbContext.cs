677beef1bf48ee6cc1349e32bf75605361e4c547
Michele Scotucci <michele.scotucci@gruppoglobalsistemi.it>
2023-05-04 10:07:43 +0200

    Add EF Core e creazione DbContext

---
+++ TestEFCore/BookStoreDbContext.cs
@@ -0,0 +1,26 @@
+﻿using EsempioADO;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestEFCore
+{
+    public class BookStoreDbContext : DbContext
+    {
+        public DbSet<Book>   Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;Initial Catalog=BookStore;Integrated Security=True;Connect Timeout=30";
+
+            optionsBuilder.UseSqlServer(connectionString);
+
+            optionsBuilder.LogTo(message=>Debug.WriteLine(message));
+        }
+    }
+}
---
+++ TestEFCore/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TestEFCore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
